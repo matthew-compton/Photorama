e4cf402d611b0f3ae2abfcef540734ffd9b90aad
matthew-compton <compton@bignerdranch.com>
2015-10-22 11:55:29 -0400

    Finished Chapter 19

---
+++ Photorama/Photo.swift
@@ -0,0 +1,26 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by Matthew Compton on 10/22/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class Photo {
+    
+    let title: String
+    let remoteURL: NSURL
+    let photoID: String
+    let dateTaken: NSDate
+    var image: UIImage?
+    
+    init(title: String, photoID: String, remoteURL: NSURL, dateTaken: NSDate) {
+        self.title = title
+        self.photoID = photoID
+        self.remoteURL = remoteURL
+        self.dateTaken = dateTaken
+    }
+    
+}
---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Matthew Compton on 10/22/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        store.fetchRecentPhotos() {
+            (photosResult) -> Void in
+            
+            switch photosResult {
+            case let .Success(photos):
+                print("Successfully found \(photos.count) recent photos.")
+                
+                if let firstPhoto = photos.first {
+                    self.store.fetchImageForPhoto(firstPhoto) {
+                        (imageResult) -> Void in
+                        
+                        switch imageResult {
+                        case let .Success(image):
+                            NSOperationQueue.mainQueue().addOperationWithBlock {
+                                self.imageView.image = image
+                            }
+                        case let .Failure(error):
+                            print("Error downloading image: \(error)")
+                        }
+                    }
+                }
+                
+            case let .Failure(error):
+                print("Error fetching recent photos: \(error)")
+            }
+            
+        }
+    }
+    
+}
